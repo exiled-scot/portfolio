159ca088be635926ed416a59cdf417b084422030
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-12 18:26:14 +0000

    css inline flex computer and text

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,9 @@ const About = () => {
         <div id="about" className="about">
             <h1 className="about-heading">About Me</h1>
             <div className='at-container'>
-                <div className='at-left'>
-                    <p className='at-text'>I am a full stack software developer and tech enthusiast from Scotland. I worked as a developer for a boutique penetration testing company near Edinburgh where I had the pleasure of securing other peoples' work against common and technically sophisticated attackers.</p>
-                    <div className='pc-monitor'>
-                        <PCMonitor />
-                    </div>
+                <p className='at-text'>I am a full stack software developer and tech enthusiast from Scotland. I worked as a developer for a boutique penetration testing company near Edinburgh where I had the pleasure of securing other peoples' work against common and technically sophisticated attackers.</p>
+                <div className='pc-monitor'>
+                    <PCMonitor />
                 </div>
             </div>
         </div>
