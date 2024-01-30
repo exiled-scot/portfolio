c9e7e9061d60da56da8ea64c6d866e196feb35fd
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-30 18:21:14 +0000

    landing page req user interaction

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,14 +3,50 @@ import './Landing.css';
 
 const Landing = () => {
   const [showPopup, setShowPopup] = useState(true);
+  const [timeoutId, setTimeoutId] = useState(null);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
+    const handleTimeout = () => {
       setShowPopup(false);
-    }, 10000); // 10 seconds
+    };
+
+    const resetTimeout = () => {
+      clearTimeout(timeoutId);
+      const newTimeoutId = setTimeout(handleTimeout, 10);
+      setTimeoutId(newTimeoutId);
+    };
+
+    const handleKeyPress = (event) => {
+      if (
+        event.key === 'Enter' ||
+        event.key === ' ' ||
+        event.key === 'ArrowUp' ||
+        event.key === 'ArrowDown' ||
+        event.key === 'ArrowLeft' ||
+        event.key === 'ArrowRight'
+      ) {
+        resetTimeout();
+      }
+    };
+
+    const handleScroll = () => {
+      resetTimeout();
+    };
+
+    const startTimeout = () => {
+      const newTimeoutId = setTimeout(handleTimeout, 10000);
+      setTimeoutId(newTimeoutId);
+    };
+
+    startTimeout();
+
+    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener('wheel', handleScroll);
 
     return () => {
-      clearTimeout(timeout);
+      clearTimeout(timeoutId);
+      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener('wheel', handleScroll);
     };
   }, []);
 
@@ -18,8 +54,8 @@ const Landing = () => {
     <>
       {showPopup && (
         <div className="popup">
-          <div class="landing-body">
-            <div class="typewriter">
+          <div className="landing-body">
+            <div className="typewriter">
               <h1>Hello, friend!</h1>
             </div>
           </div>
@@ -27,6 +63,6 @@ const Landing = () => {
       )}
     </>
   );
-}
+};
 
 export default Landing;
