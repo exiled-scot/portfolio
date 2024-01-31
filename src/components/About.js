0e168b865f9bf65fc4a1f8af300c33e5053b9ea8
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-31 20:01:16 +0000

    reworded about

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ const About = () => {
       <div className="content-container">
         <div className="text-container">
           <div className="about-text">
-            <p>I am a full stack software developer and tech enthusiast from Scotland.
+            <p>I am a full stack software developer from Scotland.
               I worked as a security tester and software developer for a boutique security company near Edinburgh.
-              By methodically testing a software engineer's pre-production code before it goes into production we would find security vulnerabilities and risks associated with bad software design.</p>
+              I tested code before it went into production, finding security vulnerabilities and risks associated with bad software design.</p>
           </div>
         </div>
         <img src={Kali} alt="Kali Linux" className="about-image" />
