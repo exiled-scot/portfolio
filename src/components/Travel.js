5b6571145715033f33399d4df61aff04782e923a
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-31 21:47:41 +0000

    font and videos

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -1,39 +1,33 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import './Travel.css';
 import sailing2 from '../assets/sailing2.png';
 import sailing3 from '../assets/sailing3.png';
 import sailing4 from '../assets/sailing4.png';
 import camping1 from '../assets/camping1.jpg';
 import dolphins1 from '../assets/dolphins.mp4';
+import sailingvid1 from '../assets/sailing-vid1.mp4';
 
 const Travel = () => {
-    const videoRef = useRef(null);
+    const dolphinsVideoRef = useRef(null);
+    const sailingvidVideoRef = useRef(null);
 
-    const handleMouseEnter = () => {
+    const handleMouseEnter = (videoRef) => {
         videoRef.current.play();
     };
 
-    const handleMouseLeave = () => {
+    const handleMouseLeave = (videoRef) => {
         videoRef.current.pause();
         videoRef.current.currentTime = 0;
     };
-
-    useEffect(() => {
-        const videoElement = videoRef.current;
-        videoElement.addEventListener('mouseenter', handleMouseEnter);
-        videoElement.addEventListener('mouseleave', handleMouseLeave);
-
-        return () => {
-            videoElement.removeEventListener('mouseenter', handleMouseEnter);
-            videoElement.removeEventListener('mouseleave', handleMouseLeave);
-        };
-    }, []);
 
     return (
         <>
             <div className='travel-container'>
                 <div className="travel-img-container">
                     <img src={sailing2} className='travel-img' alt="Sailing 2" />
+                </div>
+                <div className="travel-img-container">
+                    <video ref={sailingvidVideoRef} src={sailingvid1} className='travel-vid' loop muted onMouseEnter={() => handleMouseEnter(sailingvidVideoRef)} onMouseLeave={() => handleMouseLeave(sailingvidVideoRef)} />
                 </div>
                 <div className="travel-img-container inverted">
                     <img src={sailing3} className='travel-img' alt="Sailing 3" />
@@ -44,8 +38,8 @@ const Travel = () => {
                 <div className="travel-img-container">
                     <img src={camping1} className='travel-img' alt="Camping 1" />
                 </div>
-                <div className="travel-img-container inverted">
-                    <video ref={videoRef} src={dolphins1} className='travel-vid' loop muted />
+                <div id="dolphin1" className="travel-img-container inverted">
+                    <video ref={dolphinsVideoRef} src={dolphins1} className='travel-vid' loop muted onMouseEnter={() => handleMouseEnter(dolphinsVideoRef)} onMouseLeave={() => handleMouseLeave(dolphinsVideoRef)} />
                 </div>
             </div>
         </>
