8317f3e18f5e32cc58a5800637b7ca08ee687a90
Scott Stevenson <scott@nihilanth.co.uk>
2024-01-31 17:50:27 +0000

    progress bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,60 @@
+// Header.js
+
+import React from 'react';
 import './Header.css';
 
-const Header = () => {
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollPercentage: 0,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollHeight = documentHeight - windowHeight;
+    const scrollPercentage = (window.scrollY / scrollHeight) * 100;
+    this.setState({ scrollPercentage });
+  };
+
+  render() {
+    const { scrollPercentage } = this.state;
+
     return (
-        <div className="header">
-                <div className="header-logo">
-                    <a href="#app"><h3 className="header-name">Scott Stevenson</h3></a>
-                </div>
-                <div className="header-container">
-                    <div className="nav-item"><a href="#about">About</a></div>
-                    <div className="nav-item"><a href="#projects">Projects</a></div>
-                    <div className="nav-item"><a href="#skills">Skills</a></div>
-                    <div className="nav-item"><a href="#footer">Contact</a></div>
-                </div>
+      <div className="header">
+        <div className="header-logo">
+          <a href="#app">
+            <h3 className="header-name">Scott Stevenson</h3>
+          </a>
         </div>
-    )
+        <div className="header-container">
+          <div className="nav-item">
+            <a href="#about">About</a>
+          </div>
+          <div className="nav-item">
+            <a href="#projects">Projects</a>
+          </div>
+          <div className="nav-item">
+            <a href="#skills">Skills</a>
+          </div>
+          <div className="nav-item">
+            <a href="#footer">Contact</a>
+          </div>
+        </div>
+        <div className="progress-bar" style={{ width: `${scrollPercentage}%` }}></div>
+      </div>
+    );
+  }
 }
 
 export default Header;
