7faede9b0aa60e35e4542fefd01a2569487755a6
Scott Stevenson <scott@nihilanth.co.uk>
2024-02-01 21:12:07 +0000

    resume button

---
+++ src/components/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <a href={require('../assets/ScottStevenson-cv.pdf')} target="_blank">
+      View my Résumé
+    </a>
+  );
+};
+
+export default Resume;
