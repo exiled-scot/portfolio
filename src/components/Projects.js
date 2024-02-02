3df886ed93190d4505498eeb00983511d1ef9579
Scott Stevenson <scott@nihilanth.co.uk>
2024-02-02 20:20:23 +0000

    project card revamp

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,65 +1,64 @@
-import Project from '../assets/login.png';
+import React from 'react';
+import Slider from 'react-slick';
+import '../assets/login.png'; // Assuming this is the correct path to the login.png file
 import data from '../data/projects.json';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import './Projects.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Projects.scss';
 
 const Projects = () => {
-    const settings = {
-        dots: true,
-        infinite: false,
-        initialSlide: 0,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 600,
-                settings: {
-                    infinite: false,
-                    initialSlide: 0,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-    return (
-        <div id='projects' className='projects'>
-            <div className="title-heading">
-                <h2>PROJECTS</h2>
-            </div>
-            <div className="projects-container">
-                <Slider {...settings}>
-                    {
-                        data.map((project, key) => {
-                            return (
+  const settings = {
+    dots: true,
+    infinite: false,
+    initialSlide: 0,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          infinite: false,
+          initialSlide: 0,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-                                
-                                <div key={key} className="project">
-                                    <a
-                                        className="project-button"
-                                        target="_blank"
-                                        href={project.link}
-                                        rel="noreferrer"
-                                    >
-                                        <div className="content">
-                                        <img src={project.image} alt={project.name} />                                           <h2 className="name">{project.name}</h2>
-                                            {
-                                                project.description.length > 130
-                                                    ? <p className='description-min'>{project.description}</p>
-                                                    : <p className='description'>{project.description}</p>
-                                            }
-                                        </div>
-                                    </a>
-                                </div>
-                            )
-                        })
-                    }
-                </Slider>
+  return (
+    <div id='projects' className='projects'>
+      <div className='title-heading'>
+        <h2>PROJECTS</h2>
+      </div>
+      <div className='projects-container'>
+        <Slider {...settings}>
+          {data.map((project, key) => (
+            <div className='example-2 card' key={key}>
+              <div
+                className='wrapper'
+                style={{
+                  backgroundImage: `url(${project.image})`,
+                  backgroundPosition: 'center center',
+                  backgroundSize: 'cover',
+                }}
+              >
+                <div className='data'>
+                  <div className='content'>
+                    <span className='type'>Project</span>
+                    <h1 className='title'>
+                      <a href={project.link}>{project.name}</a>
+                    </h1>
+                    <p className='text'>{project.description}</p>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    )
-}
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
