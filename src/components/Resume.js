c6dd29e2d182d9b219b33d2d74d8bc8a4ad58830
Scott Stevenson <scott@nihilanth.co.uk>
2024-06-10 17:03:41 +0100

    reactive-resume cv link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Resume = () => {
   return (
-    <a href={require('../assets/ScottStevenson-cv.pdf')} target="_blank">
+      <a href={require('https://storage.rxresu.me/clw5ahyzs067oo3vhmgk46k1i/resumes/Full%20Stack%20Developer.pdf')} target="_blank">
       View my Résumé
     </a>
   );
